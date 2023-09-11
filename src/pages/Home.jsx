409bacf42a63b5b7afca9a3ada7a27c0e2d63172
judiciousmurich <murichjudicious45@gmail.com>
2023-09-11 23:19:42 +0300

    :feat:add home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-
+import { Link} from 'react-router-dom'
 
 const Home = () => {
   return (
   
    <>
-    <div className="bg-gradient-to-r from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] bg-cover bg-center min-h-screen flex items-center justify-center bg-no-repeat">
+   <div>
+   <div className="bg-gradient-to-r from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] bg-cover bg-center min-h-screen flex items-center justify-center bg-no-repeat">
   <img src="http://127.0.0.1:5501/media/images/breakfast.jpg" alt="breakfast"  />
-
   </div>
+
   
-  <h1 className="text-4xl font-bold">Welcome to My Website</h1>
-  <p className="text-lg mt-4">This is the homepage of my website.</p>
+<div>
+  <h2>Good food makes you live good life.</h2>
+  <div>
+    <Link to='/'>Eat Now</Link>
+    <Link to='/'>Keep reading</Link>
+  </div>
+</div>
+   </div>
+   
+
 
 </>
   );
