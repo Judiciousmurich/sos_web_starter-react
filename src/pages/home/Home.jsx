e2f541811e42b6bb9f8e782e70d806ff0d61262d
judiciousmurich <murichjudicious45@gmail.com>
2023-09-12 22:46:21 +0300

    feat:add home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,27 @@
-import { Link} from 'react-router-dom'
-import'./Home.css'
+import { Link } from 'react-router-dom'
+import './Home.css'
 const Home = () => {
   return (
-  
-   <>
-   <div>
-   <div className="home">
-   <div>
-  <h2 className='text-white font-bold text-5xl mb-4'>Good food makes you live good life.</h2>
-  <div className='gap-6'>
-    <Link to='/' className='bg-[#b10400] text-white px-3 py-2 rounded  font-bold '>Eat Now</Link>
-    <Link to='/'className='text-white font bold px-6 py-2 rounded border-white '>Keep reading</Link>
-  </div>
-</div>
-   </div>
-  </div> 
 
-  
-
-   
+    <>
+      <div>
+        <div className="home">
+          <div>
+            <h2 className='text-white font-bold text-5xl mb-4 capitalize'>Good food makes you life good life. </h2>
+            <div className='gap-6'>
+              <Link to='/' className='bg-[#b10400] text-white px-3 py-2 rounded  font-bold '>Eat Now</Link>
+              <Link to='/' className='text-white font bold px-6 py-2 rounded'>Keep reading</Link>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
-</>
+
+
+
+
+    </>
   );
 };
 
--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -4,33 +4,33 @@ import './navbar.css'
 
 export const Navbar = () => {
     return (
-           <div className='navbar'>
+        <div className='navbar'>
             <div className='font-bold text-2xl text-[#b10400]  p-4'>
-            <Link to="/">Sea Of Soup</Link>
+                <Link to="/">Sea Of Soup</Link>
             </div>
-               
-                <nav className='links'>
-                    <NavLink to="/" >
-                        Home
-                    </NavLink>
-                    <NavLink to="/menu" >
-                        Menu
-                    </NavLink>
-                    <NavLink to="/order" >
-                        Order
-                    </NavLink>
-                    <NavLink to="/team" >
-                        Team
-                    </NavLink>
-                    <NavLink to="/testimonials" >
-                        Testimonials
-                    </NavLink>
-                    <NavLink to="/blog" >
-                        Blog
-                    </NavLink>
-                </nav>
+
+            <nav className='links'>
+                <NavLink to="/" >
+                    Home
+                </NavLink>
+                <NavLink to="/menu" >
+                    Menu
+                </NavLink>
+                <NavLink to="/order" >
+                    Order
+                </NavLink>
+                <NavLink to="/team" >
+                    Team
+                </NavLink>
+                <NavLink to="/testimonials" >
+                    Testimonials
+                </NavLink>
+                <NavLink to="/blog" >
+                    Blog
+                </NavLink>
+            </nav>
 
 
-                </div >
+        </div >
     )
 }
