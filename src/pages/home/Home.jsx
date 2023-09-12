dd31c590eaef0e3ec87869ba95c5b5075780a4fe
judiciousmurich <murichjudicious45@gmail.com>
2023-09-12 15:48:14 +0300

    feat:add styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
    <div className="home">
    <div>
   <h2 className='text-white font-bold text-5xl mb-4'>Good food makes you live good life.</h2>
-  <div className=''>
+  <div className='gap-6'>
     <Link to='/' className='bg-[#b10400] text-white px-3 py-2 rounded  font-bold '>Eat Now</Link>
-    <Link to='/'>Keep reading</Link>
+    <Link to='/'className='text-white font bold px-6 py-2 rounded border-white '>Keep reading</Link>
   </div>
 </div>
    </div>
