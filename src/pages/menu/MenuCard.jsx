8e74bd09ae1e2c5cea1c16eb5bcfd07981076812
judiciousmurich <murichjudicious45@gmail.com>
2023-09-15 18:20:57 +0300

    feat:add menu styles

--- src/pages/menu/MenuCard.jsx
+++ src/pages/menu/MenuCard.jsx
@@ -2,7 +2,7 @@ import "./menu.css";
 import { Link } from "react-router-dom";
 const MenuCard = ({ image, title, description, price }) => {
   return (
-    <div className="menu-card">
+    <div className="menu-card bg-[#dde0e0] items-center justify-center m-2">
       <div className="menu-card-image-container">
         <img src={image} alt={title} className="menu-card-image" />
       </div>
@@ -10,7 +10,7 @@ const MenuCard = ({ image, title, description, price }) => {
         {title}
       </h3>
       <p className="menu-card-description text-center tracking-wide text-base row-start mb-4">{description}</p>
-      <Link to ="" className="menu-card-cta rounded-[30px] px-4 py-2 bg-[#b10400] text-white capitalize text-base">
+      <Link to ="" className="menu-card-cta rounded-[30px] px-4 py-2 bg-[#b10400] text-white capitalize text-base m-6 ">
         ${price} | order now
       </Link>
     </div>
