c8d4bbaba19b0d1ca0d8d49689860dfc3a72f1d6
judiciousmurich <murichjudicious45@gmail.com>
2023-09-15 14:33:36 +0300

    feat:add menuCard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,28 @@
-import { Link } from 'react-router-dom';
-import backgroundImage from '../../media/images/breakfast.jpg'; 
-
+import { Link } from 'react-router-dom'
+import './Home.css'
 const Home = () => {
   return (
-    <div className="home bg-gradient-to-r from-[rgba(0,0,0,0.5)] via-[rgba(0,0,0,0.5)] to-transparent min-h-screen relative">
-      <div className="absolute inset-0 bg-gradient-to-r from-[rgba(0,0,0,0.5)] via-[rgba(0,0,0,0.5)] to-transparent"></div>
-      <div className="flex justify-center items-center h-full relative z-10">
-        <div className="text-center text-white">
-          <h2 className="font-bold text-5xl mb-2 capitalize">Good food makes your life</h2>
-          <h2 className="font-bold mb-8 text-5xl capitalize">good life.</h2>
-          <div className="gap-6 text-center">
-            <Link to="/" className="bg-[#b10400] text-white px-3 py-2 rounded font-bold text-center">Eat Now</Link>
-            <Link to="/" className="text-white font-bold px-6 py-2 rounded border-white">Keep reading</Link>
+
+    <>
+      <div>
+        <div className="home">
+          <div>
+            <h2 className='text-white font-bold text-5xl mb-2 capitalize'>Good food makes you life </h2>
+            <h2 className='text-center font-bold mb-8 text-5xl  capitalize text-white'>good life. </h2>
+            <div className='gap-6 text-center'>
+              <Link to='/' className='bg-[#b10400] text-white px-3 py-2 rounded  font-bold  text-center'>Eat Now</Link>
+              <Link to='/' className='text-white font bold px-6 py-2 rounded border-white'>Keep reading</Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="absolute inset-0" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
-    </div>
+
+
+
+
+
+
+    </>
   );
 };
 
---
+++ src/pages/home/menu/MenuCard.jsx
@@ -0,0 +1,12 @@
+const MenuCard = ({ image, title, description, price }) => {
+    return (
+      <div className="menu-card">
+        <div className="menu-card-image-container">
+          <img src={image} alt={title} className="menu-card-image" />
+        </div>
+        <h3 className="menu-card-title">{title}</h3>
+        <p className="menu-card-description">{description}</p>
+        <a href="#" className="menu-card-cta">${price} | order now</a>
+      </div>
+    );
+  };
