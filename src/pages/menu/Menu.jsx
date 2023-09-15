c02dcf1c61981c95f9d16c4bc7d5691e993473e0
judiciousmurich <murichjudicious45@gmail.com>
2023-09-15 16:32:35 +0300

    feat:add menu stylings

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,16 +1,19 @@
-import'./menu.css'
+import "./menu.css";
 const Menu = ({ image, title, description, price }) => {
   return (
     <div className="menu-card">
       <div className="menu-card-image-container">
         <img src={image} alt={title} className="menu-card-image" />
       </div>
-      <h3 className="menu-card-title  capitalize font-bold text-2x">{title}</h3>
+      <h3 className="menu-card-title  capitalize font-bold text-2xl">
+        {title}
+      </h3>
       <p className="menu-card-description ">{description}</p>
-      <a href="#" className="menu-card-cta">${price} | order now</a>
+      <a href="#" className="menu-card-cta">
+        ${price} | order now
+      </a>
     </div>
   );
 };
 
 export default Menu;
-l
