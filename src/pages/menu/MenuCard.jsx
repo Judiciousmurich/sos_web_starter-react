e6b9f0423dc09c309d9d083b1540fb579978f072
judiciousmurich <murichjudicious45@gmail.com>
2023-09-15 18:05:56 +0300

    feat:add menu styles

--- src/pages/menu/MenuCard.jsx
+++ src/pages/menu/MenuCard.jsx
@@ -6,11 +6,11 @@ const MenuCard = ({ image, title, description, price }) => {
       <div className="menu-card-image-container">
         <img src={image} alt={title} className="menu-card-image" />
       </div>
-      <h3 className="menu-card-title  capitalize font-bold text-2xl">
+      <h3 className="menu-card-title  capitalize font-bold text-2xl mb-4">
         {title}
       </h3>
-      <p className="menu-card-description text-center tracking-wide text-base row-start">{description}</p>
-      <Link to ="" className="menu-card-cta">
+      <p className="menu-card-description text-center tracking-wide text-base row-start mb-4">{description}</p>
+      <Link to ="" className="menu-card-cta  font-bold rounded px-4 py-2 bg-[#b10400] text-white">
         ${price} | order now
       </Link>
     </div>
