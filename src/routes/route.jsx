391686cf5692369b2ac21dab943c0ea53dbcbe33
judiciousmurich <murichjudicious45@gmail.com>
2023-09-15 15:00:32 +0300

    feat:add menu routing

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import Home from '../pages/home/Home';
+import Menu from '../pages/menu/Menu';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="" element={<Layout />}>
         <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Menu/>} />
       </Route>
     </Route>
   )
