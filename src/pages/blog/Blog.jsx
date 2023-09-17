06ca79ae5f174714fea366a9c2fda767ef75bb86
judiciousmurich <murichjudicious45@gmail.com>
2023-09-17 23:59:25 +0300

    feat:add styles

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -20,6 +20,83 @@ const Blog= [
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
   },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
   // Add more blog items as needed
 ];
 
