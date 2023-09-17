c590e1e9012539df0bd23ba973c1ceffbe74210a
judiciousmurich <murichjudicious45@gmail.com>
2023-09-17 23:20:47 +0300

    feat:add styles

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import BlogCard from './BlogCard'
+const Blog= [
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  {
+    imageUrl: './media/images/dessert.jpg',
+    date: '23rd January 2023',
+    title: 'How to Prepare the Best Dessert',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id ex soluta unde animi velit excepturi. Fugiat nam aperiam sunt natus.',
+  },
+  // Add more blog items as needed
+];
 
-const Blog = () => {
-  return (
-    <BlogCard
-
-
-  )
-}
-
-export default Blog
-
-
-
-
-
+export default Blog;
