65e61c78aea073714f4d002855ee4e5c9fa24c6b
judiciousmurich <murichjudicious45@gmail.com>
2023-09-19 23:57:48 +0300

    feat:add menu styles

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -14,7 +14,7 @@ const Menu = () => {
       image="http://127.0.0.1:5501/media/images/dessert.jpg"
       title="Drinks"
       description="Refreshing drinks that quench your thirst with a burst of flavor and cool satisfaction on a hot day."
-      price="25"
+      price="22"
     />
     <MenuCard
       image="http://127.0.0.1:5501/media/images/breakfast.jpg"
